5811da575872e1f80ae4fd47fd8d11432fcd7ed0
yudhisthereal <63510362+yudhisthereal@users.noreply.github.com>
2023-08-31 23:59:24 +0700

    Login UI

--- app/src/main/java/com/ggroup/greecle/MainActivity.kt
+++ app/src/main/java/com/ggroup/greecle/MainActivity.kt
@@ -2,10 +2,14 @@ package com.ggroup.greecle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
     }
 }
