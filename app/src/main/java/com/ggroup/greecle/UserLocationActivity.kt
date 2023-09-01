b613a2708a2a355e331541f3c248f2a4b1c5b17b
yudhisthereal <63510362+yudhisthereal@users.noreply.github.com>
2023-09-01 20:17:17 +0700

    Layout for static pages

---
+++ app/src/main/java/com/ggroup/greecle/UserLocationActivity.kt
@@ -0,0 +1,11 @@
+package com.ggroup.greecle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserLocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_location)
+    }
+}
